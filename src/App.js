77380c41d031efde9b5b2e714f3504a3411230a8
M-Rb3 <rabea9220@gmail.com>
2021-06-03 19:37:56 +0200

    solving error

--- src/App.js
+++ src/App.js
@@ -8,8 +8,6 @@ import Nav from "./components/Nav";
 import Player from "./components/Player";
 import Song from "./components/Song";
 import Library from "./components/Library";
-import { Component } from "react";
-import { library } from "@fortawesome/fontawesome-svg-core";
 
 function App() {
   // ref
