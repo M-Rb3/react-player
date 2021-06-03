9e697af3c2057a29054183074b1f2c8f4d59d638
M-Rb3 <rabea9220@gmail.com>
2021-06-03 19:46:32 +0200

    error 3

--- src/components/Player.js
+++ src/components/Player.js
@@ -34,7 +34,7 @@ const Player = ({
         song.active = false;
       }
       if (isPlaying) audioRef.current.play();
-      return none;
+      return null;
     });
   };
   const playSongHandler = () => {
